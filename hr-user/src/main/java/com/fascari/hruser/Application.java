0cdd69e8319d539aadd44370c79af3745d64fb01
Felipe Ascari <felipe.ascari@gmail.com.br>
2021-10-22 16:48:50 -0300

    Ajuste passwordencoder

--- hr-user/src/main/java/com/fascari/hruser/Application.java
+++ hr-user/src/main/java/com/fascari/hruser/Application.java
@@ -1,15 +1,25 @@
 package com.fascari.hruser;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 @EnableEurekaClient
-public class Application {
+public class Application implements CommandLineRunner {
+
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
+    @Override
+    public void run(String... args) {
+        //System.out.println("BCrypt:" + passwordEncoder.encode("abc.123"));
+    }
 }
