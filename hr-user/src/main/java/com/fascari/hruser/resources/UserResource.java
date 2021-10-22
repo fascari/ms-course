6f560f07af734644cd8ea931bd59ce41e86d331c
Felipe Ascari <felipe.ascari@gmail.com.br>
2021-10-22 17:05:46 -0300

    UserRepository, UserResource, Zuul config

--- hr-user/src/main/java/com/fascari/hruser/resources/UserResource.java
+++ hr-user/src/main/java/com/fascari/hruser/resources/UserResource.java
@@ -3,8 +3,10 @@ package com.fascari.hruser.resources;
 import com.fascari.hruser.entities.User;
 import com.fascari.hruser.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -16,7 +18,7 @@ public class UserResource {
     @GetMapping(value = "/{id}")
     public ResponseEntity<User> findById(@PathVariable Long id) {
         User obj = repository.findById(id)
-                .orElseThrow(() -> new RuntimeException("Usuário não encontrado na base de dados!"));
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Usuário não encontrado na base de dados!"));
         return ResponseEntity.ok(obj);
     }
 
