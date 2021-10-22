db610bfe937d2563c28fbe3fc34f4e477a70e49c
Felipe Ascari <felipe.ascari@gmail.com.br>
2021-10-22 16:08:12 -0300

    hr-worker refreshscope actuator

--- hr-worker/src/main/java/com/fascari/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/fascari/hrworker/resources/WorkerResource.java
@@ -1,21 +1,22 @@
 package com.fascari.hrworker.resources;
 
-import java.util.List;
-
+import com.fascari.hrworker.entities.Worker;
+import com.fascari.hrworker.repositories.WorkerRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import com.fascari.hrworker.entities.Worker;
-import com.fascari.hrworker.repositories.WorkerRepository;
 
-//@RefreshScope
+import java.util.List;
+
+@RefreshScope
 @RestController
 @RequestMapping(value = "/workers")
 public class WorkerResource {
