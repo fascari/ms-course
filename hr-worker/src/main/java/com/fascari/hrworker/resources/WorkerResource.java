25000b663429ebceeab4b0fc4667cb91bf5e4444
Felipe Ascari <felipe.ascari@gmail.com.br>
2021-11-01 16:44:51 -0300

    Config docker postgres hr-worker e hr-user

--- hr-worker/src/main/java/com/fascari/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/fascari/hrworker/resources/WorkerResource.java
@@ -5,7 +5,6 @@ import com.fascari.hrworker.repositories.WorkerRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
@@ -23,9 +22,6 @@ public class WorkerResource {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(WorkerResource.class);
 
-    @Value("${spring.datasource.url}")
-    private String testConfig;
-
     @Autowired
     private Environment env;
 
@@ -34,7 +30,7 @@ public class WorkerResource {
 
     @GetMapping(value = "/configs")
     public ResponseEntity<Void> getConfigs() {
-        LOGGER.info("CONFIG = " + testConfig);
+        //LOGGER.info("CONFIG = " + testConfig);
         return ResponseEntity.noContent().build();
     }
 
@@ -46,15 +42,12 @@ public class WorkerResource {
 
     @GetMapping(value = "/{id}")
     public ResponseEntity<Worker> findById(@PathVariable Long id) {
-
-        try {
+        /*try {
             Thread.sleep(3000L);
         } catch (InterruptedException e) {
             e.printStackTrace();
-        }
-
+        }*/
         LOGGER.info("PORT = " + env.getProperty("local.server.port"));
-
         Worker obj = repository.findById(id).get();
         return ResponseEntity.ok(obj);
     }
